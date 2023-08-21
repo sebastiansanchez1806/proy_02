68b1311624d0e1d1beb412dcb551df78d31393a4
Johan024 <jevergara987@misena.edu.co>
2023-08-21 10:37:30 -0500

    cambios

---
+++ app.js
@@ -0,0 +1,9 @@
+const functions = require('./functions.js')
+
+console.log(functions);
+process.stdout.write("Ejercicio\n\n");
+
+console.log(functions.add(8,4));
+console.log(functions.substract(3,9));
+console.log(functions.multiply(4,6));
+console.log(functions.divide(3,0.9));
