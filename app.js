f2a7a438c36de0950f98566a4e7f87de01ccaa8b
Johan024 <jevergara987@misena.edu.co>
2023-09-15 19:44:48 -0500

    cambios

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+//Requerimos a functions
 const functions = require('./functions.js')
 
+//Mandamos a imprimir las functions
 console.log(functions);
+
+//Imprime el mensaje Ejercicio en la consola
 process.stdout.write("Ejercicio\n\n");
 
+//Le damos valor a las functions y las mandamos imprimir
 console.log(functions.add(8,4));
 console.log(functions.substract(3,9));
 console.log(functions.multiply(4,6));
--- functions.js
+++ functions.js
@@ -1,16 +1,22 @@
+//Se define constante de funciones matematicas
 const Math = {};
 
+//Define la funcion suma(add)
 function add(a,b){
     return a + b;
 }
+
+//Define la funcion Restar(substract)
 function substract(a,b){
     return a - b;
 }
 
+//Define la funcion Multiplicar(multiply)
 function multiply(a,b){
     return a * b;
 }
 
+//Define la funcion Dividir(divide)
 function divide(a,b){
     if (b == 0) {
         return "ERROR NO SE PUEDE DIVIDIR POR CERO"
@@ -18,8 +24,12 @@ function divide(a,b){
         return a / b;
     }
     }
+
+    //Definimos las funciones
     Math.add = add;
     Math.substract = substract;
     Math.multiply = multiply;
     Math.divide = divide;
+
+    //Exporta todo el modulo Math
     module.exports = Math;
