5df013abad4018681d39a55e242e5fbb6ffe38cc
sebastiansanchez1806 <barrantessebastian261@gmail.com>
2023-10-18 13:32:58 -0500

    guia 2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ console.log(functions);
 process.stdout.write("Ejercicio\n\n");
 
 //Le damos valor a las functions y las mandamos imprimir
-console.log(functions.add(8,4));
-console.log(functions.substract(3,9));
-console.log(functions.multiply(4,6));
-console.log(functions.divide(3,0.9));
+console.log(functions.add(8,16));
+console.log(functions.substract(320,9));
+console.log(functions.multiply(9,6));
+console.log(functions.divide(3,0.12));
